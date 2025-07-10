2d28274dd34d9feb25820bc46d878937e1e6acb7
ThuyLinh110 <thuylinh1102001@gmail.com>
2025-07-10 17:53:31 +0700

    implement tc04

--- src/main/java/org/example/utils/WebUtils.java
+++ src/main/java/org/example/utils/WebUtils.java
@@ -1,12 +1,17 @@
 package org.example.utils;
 
 import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
 import com.codeborne.selenide.WebDriverRunner;
+import org.openqa.selenium.Keys;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static com.codeborne.selenide.Selenide.actions;
+import static com.codeborne.selenide.Selenide.sleep;
 
 public class WebUtils {
     private static final Logger log = LoggerFactory.getLogger(WebUtils.class);
@@ -19,6 +24,21 @@ public class WebUtils {
         List<String> handleList = new ArrayList<>(WebDriverRunner.getWebDriver().getWindowHandles());
         int currentIndex = handleList.indexOf(WebDriverRunner.getWebDriver().getWindowHandle());
         Selenide.switchTo().window(handleList.get(currentIndex - 1));
+    }
+
+    public static void scrollUntilElementVisible(SelenideElement element) {
+        int maxTries = 50;
+        int tries = 0;
+
+        while (!element.exists() && tries < maxTries) {
+            actions().sendKeys(Keys.PAGE_DOWN).perform();
+            sleep(500); // Wait for lazy-load
+            tries++;
+        }
+        actions().sendKeys(Keys.PAGE_DOWN).perform();
+        if (!element.exists()) {
+            throw new RuntimeException("Element not found after scrolling");
+        }
     }
 
 }
